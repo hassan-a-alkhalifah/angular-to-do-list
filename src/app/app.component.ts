80c05d4c7c8b0cbe4dd9af6ac049d1b203c342d9
Colin Cook and Hassan Al-khalifah <colin@>
2018-10-17 11:58:33 -0700

    Add task-list and edit-task components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  taskList: Task[] = [
+  selectedTask = null;
+
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
